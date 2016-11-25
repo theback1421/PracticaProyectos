da03c1dda133129a7bff3c54758d995aca522d6a
Ale0710 <alejandrafa60@gmail.com>
2016-11-25 09:58:36 -0600

    Update Practicas.java

--- Practicas.java
+++ Practicas.java
@@ -30,6 +30,25 @@ public class Practicas
     
     public static void fibonacci ()
     {
+        Scanner sc = new Scanner(System.in);
+        int numero,fibo1,fibo2,i;
+        do{
+            System.out.print("Ingrese un numero mayor que 1: ");
+            numero = sc.nextInt();
+        }while(numero<=1);
+        System.out.println("Los " + numero + " primeros términos de la serie de Fibonacci son:"); 
+
+        fibo1=1;
+        fibo2=1; 
+
+        System.out.print(fibo1 + " ");
+        for(i=2;i<=numero;i++){
+             System.out.print(fibo2 + " ");
+             fibo2 = fibo1 + fibo2;
+             fibo1 = fibo2 - fibo1;
+        }
+        System.out.println();
+    }
         //Fibonacci de X número.
     }
     
